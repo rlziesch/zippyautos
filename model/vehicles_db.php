5212485e3bd222670bd22fbacea7cdcc182c91b6
Rebekah <rebekah.kivett@gmail.com>
2021-03-15 22:05:17 -0500

    add missing functions to vehicles_db

--- model/vehicles_db.php
+++ model/vehicles_db.php
@@ -22,16 +22,59 @@ function get_vehicles($vehicle_id) {
 function get_vehicle_by_class($class_id) {
     global $db;
         $query = 'SELECT * FROM vehicles
+                INNER JOIN types
+                ON vehicles.type_ID = types.typeID
+                INNER JOIN makes
+                ON vehicles.make_ID = makes.make_ID
                 INNER JOIN classes
                 ON vehicles.class_ID = classes.class_ID
+                WHERE class_ID = :class_id
                 ORDER BY vehicles.Price DESC';
          $statement = $db->prepare($query);
+         $statement->bindValue('class_id', $class_id);
          $statement->execute();
-         $vehicles_by_class = $statement->fetchAll();
+         $vehicles = $statement->fetchAll();
          $statement->closeCursor();
-         return $vehicles_by_class;
+         return $vehicles;
 }
 
+function get_vehicle_by_type($type_id) {
+    global $db;
+        $query = 'SELECT * FROM vehicles
+                INNER JOIN types
+                ON vehicles.type_ID = types.typeID
+                INNER JOIN makes
+                ON vehicles.make_ID = makes.make_ID
+                INNER JOIN classes
+                ON vehicles.class_ID = classes.class_ID
+                WHERE typeID = :type_id
+                ORDER BY vehicles.Price DESC';
+         $statement = $db->prepare($query);
+         $statement->bindValue('type_id', $type_id);
+         $statement->execute();
+         $vehicles = $statement->fetchAll();
+         $statement->closeCursor();
+         return $vehicles;
+}
+
+function get_vehicle_by_make($make_id) {
+    global $db;
+        $query = 'SELECT * FROM vehicles
+                INNER JOIN types
+                ON vehicles.type_ID = types.typeID
+                INNER JOIN makes
+                ON vehicles.make_ID = makes.make_ID
+                INNER JOIN classes
+                ON vehicles.class_ID = classes.class_ID
+                WHERE make_ID = :make_id
+                ORDER BY vehicles.Price DESC';
+         $statement = $db->prepare($query);
+         $statement->bindValue('make_id', $make_id);
+         $statement->execute();
+         $vehicles = $statement->fetchAll();
+         $statement->closeCursor();
+         return $vehicles;
+}
 
 
 ?>
